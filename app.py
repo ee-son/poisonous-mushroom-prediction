07d181b1288d40b32520b69cb83b06bad9b32359
ee-son <isan.faizun.hidar@mail.ugm.ac.id>
2025-03-24 09:27:48 +0700

    feat: complete the result

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import os
+import pandas as pd
 import joblib
 from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
+variable_form = ['cap-shape', 'cap-surface', 'cap-color', 'bruises', 'odor',
+                 'gill-attachment', 'gill-spacing', 'gill-size', 'gill-color',
+                 'stalk-shape', 'stalk-root', 'stalk-surface-above-ring', 'stalk-surface-below-ring',
+                 'stalk-color-above-ring', 'stalk-color-below-ring', 'veil-type', 'veil-color',
+                 'ring-number', 'ring-type', 'spore-print-color', 'population', 'habitat']
 
 @app.route('/')
 def index():
@@ -10,6 +16,7 @@ def index():
 
 @app.route('/result/', methods=["POST"])
 def result():
+    # Get features from form
     cap_shape = request.form.get('cap-shape')
     cap_surface = request.form.get('cap-surface')
     cap_color = request.form.get('cap-color')
@@ -32,12 +39,40 @@ def result():
     spore_print_color = request.form.get('spore-print-color')
     population = request.form.get('population')
     habitat = request.form.get('habitat')
+
+    variable_names = [cap_shape, cap_surface, cap_color, bruises, odor,
+                  gill_attachment, gill_spacing, gill_size, gill_color,
+                  stalk_shape, stalk_root, stalk_surface_above_ring, stalk_surface_below_ring,
+                  stalk_color_above_ring, stalk_color_below_ring, veil_type, veil_color,
+                  ring_number, ring_type, spore_print_color, population, habitat]
     
     # Load model
-    filepath = '/model/rf_model.model'
+    filepath = 'model/rf_model.model'
     load_model = joblib.load(filepath)
 
-    return render_template('result.html')
+    # Make new dataframe for input
+    df_input = pd.DataFrame(columns = variable_form)
+    df_input.loc[0] = variable_names
+
+    # print result
+    result = load_model.predict(df_input)
+    for i in result:
+        int_result = int(i)
+        if(int_result==0):
+            decision="Definitely poisonous"
+        elif(int_result==1):
+            decision="It's not poisonous"
+        else:
+            decision="We don't know"
+
+    # return output
+    return render_template('result.html', cap_shape=cap_shape, cap_surface=cap_surface, cap_color=cap_color, 
+                           bruises=bruises, odor=odor, gill_attachment=gill_attachment, gill_spacing=gill_spacing, 
+                           gill_size=gill_size, gill_color=gill_color, stalk_shape=stalk_shape, stalk_root=stalk_root, 
+                           stalk_surface_above_ring=stalk_surface_above_ring, stalk_surface_below_ring=stalk_surface_below_ring, 
+                           stalk_color_above_ring=stalk_color_above_ring, stalk_color_below_ring=stalk_color_below_ring, 
+                           veil_type=veil_type, veil_color=veil_color, ring_number=ring_number, ring_type=ring_type, 
+                           spore_print_color=spore_print_color, population=population, habitat=habitat, decision=decision)
 
 if __name__ == "__main__":
     app.run()
